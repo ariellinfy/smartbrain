2cdd79160533b6766a2f7025304a8563405a7bcd
ariellinfy <ariellinfy@gmail.com>
2020-12-29 13:21:36 -0700

    testing clarify api and re-deploy

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './FaceRecognition.css'
+import './FaceRecognition.css';
 
 const FaceRecognition = ({ imageUrl, box }) => {
 	return (
 		<div className = 'center ma'>
-			<div className = 'absolute mv2 pt3 pb5'>
+			<div className = 'relative mt3'>
 				<img id='inputImage' alt='' src= {imageUrl} width='500px' height='auto'/>
 				<div className = 'bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
 			</div>
